ad72fb3994b88911ba12bea08afbf37596cf13e1
abemartinez2020 <amm172005@gmail.com>
2021-06-03 23:20:20 -0500

    configured SASS

--- webpack.config.js
+++ webpack.config.js
@@ -1,31 +1,31 @@
 const path = require('path');
-module.exports = {
-    entry: {
-        index: ['core-js', 'regenerator-runtime/runtime', './src/index.js'],
-        form: ['core-js', 'regenerator-runtime/runtime', './src/form.js']
-    },
-    
-    output: {
-        path: path.resolve(__dirname, 'public/scripts'),
-        filename: '[name]-bundle.js'
-    }, 
-    module: {
-        rules: [{
-            test:/\.js$/,
-            exclude: /node_modules/,
-           use: {
-               loader: 'babel-loader',
-               options: {
-                   presets: ['@babel/env'],
-                   plugins: ['transform-object-rest-spread']
-               }
-           } 
-        }]
-    },
-    devServer: {
-        contentBase: path.resolve(__dirname, 'public'),
-        publicPath: '/scripts/'
-    },
-    devtool: 'source-map'
-}
 
+module.exports = {
+  entry: {
+    index: ['core-js', 'regenerator-runtime/runtime', './src/index.js'],
+    form: ['core-js', 'regenerator-runtime/runtime', './src/form.js'],
+  },
+
+  output: {
+    path: path.resolve(__dirname, 'public/scripts'),
+    filename: '[name]-bundle.js',
+  },
+  module: {
+    rules: [{
+      test: /\.js$/,
+      exclude: /node_modules/,
+      use: {
+        loader: 'babel-loader',
+        options: {
+          presets: ['@babel/env'],
+          plugins: ['transform-object-rest-spread'],
+        },
+      },
+    }],
+  },
+  devServer: {
+    contentBase: path.resolve(__dirname, 'public'),
+    publicPath: '/scripts/',
+  },
+  devtool: 'source-map',
+};
